3db3ded1db50eef3237e1f533f9e2eb09fd68f5f
Judson Stevens <Judson.Stevens@gmail.com>
2018-04-17 11:07:15 -0600

    Working on auditing, WIP

--- lib/audit.rb
+++ lib/audit.rb
@@ -15,17 +15,23 @@ class Audit
   # end
 
   def check_timesheet_dates_for_weekend
-    @company.timesheets.each do |timesheet|
-      return 'weekend' if timesheet.date.wday == 6 || timesheet.date.wday == 7
+    @company.timesheets.map do |timesheet|
+      if timesheet.date.wday == 6 || timesheet.date.wday == 7
+        return 'weekend'
+      else
+        return 'none'
+      end
     end
   end
 
   def check_timesheet_date_for_invalid_billing
-    @company.timesheets.each do |timesheet|
+    @company.timesheets.map do |timesheet|
       dh = DateHandler::DHDate.new(timesheet.date)
       project = @company.find_project_by_id(timesheet.project_id)
       if !dh.date_between(project.start_date, project.end_date)
         return 'invalid billing'
+      else
+        return 'none'
       end
     end
   end
--- test/audit_test.rb
+++ test/audit_test.rb
@@ -5,10 +5,6 @@ require './lib/company'
 class AuditTest < MiniTest::Test
 
   def setup
-    @c = Company.new
-    @c.load_projects('./data/projects.csv')
-    @c.load_employees('./data/employees.csv')
-    @c.load_timesheets('./data/good_timesheets.csv')
     @a = Audit.new
   end
 
@@ -17,14 +13,29 @@ class AuditTest < MiniTest::Test
   end
 
   def test_load_company_returns_a_company
+    c = Company.new
     expected = Company
-    actual = @a.load_company(@c).class
+    actual = @a.load_company(c).class
 
     assert_equal expected, actual
   end
 
   def test_it_can_find_weekends
-    @a.load_company(@c)
+    c = Company.new
+    c.load_projects('./data/projects.csv')
+    c.load_timesheets('./data/good_timesheets.csv')
+    @a.load_company(c)
+
+    expected = 'none'
+    actual = @a.check_timesheet_dates_for_weekend
+
+    assert_equal expected, actual
+
+    c1 = Company.new
+    c1.load_projects('./data/projects.csv')
+    c1.load_timesheets('./data/weekend_timesheet.csv')
+    @a.load_company(c1)
+
     expected = 'weekend'
     actual = @a.check_timesheet_dates_for_weekend
 
@@ -32,9 +43,12 @@ class AuditTest < MiniTest::Test
   end
 
   def test_it_can_find_invalid_start_date
-    @a.load_company(@c)
-    expected = 'invalid billing'
-    require "pry"; binding.pry
+    c = Company.new
+    c.load_projects('./data/projects.csv')
+    c.load_timesheets('./data/good_timesheets.csv')
+    @a.load_company(c)
+
+    expected = 'none'
     actual = @a.check_timesheet_date_for_invalid_billing
 
     assert_equal expected, actual
