492a127da5c71ba34ed3f120da8225340b53ab84
Judson Stevens <Judson.Stevens@gmail.com>
2018-04-17 11:51:33 -0600

    Working on checking for valid project id

--- lib/audit.rb
+++ lib/audit.rb
@@ -11,14 +11,24 @@ class Audit
   end
 
   def were_invalid_days_worked
-    return return_message_for_weekend if return_message_for_weekend != []
-    return return_message_for_invalid_date if return_message_for_invalid_date != []
+    return return_message_for_weekend if return_message_for_weekend != nil
+    return return_message_for_invalid_billing if return_message_for_invalid_billing != nil
     "Invalid Days Worked: \nNone"
   end
 
   def return_message_for_weekend
     check_timesheet_dates_for_weekend.map do |response|
-      if response.class == Hash
+      if !response.nil? && response[:reason] == 'weekend'
+        employee = @company.find_employee_by_id(response[:employee_id])
+        project = @company.find_project_by_id(response[:project_id])
+        "Invalid Days Worked: \n#{employee.name} worked on #{project.name} on #{response[:date]}, it was a(n) #{response[:reason]}."
+      end
+    end.compact[0]
+  end
+
+  def return_message_for_invalid_billing
+    check_timesheet_date_for_invalid_billing.map do |response|
+      if !response.nil? && response[:reason] == 'invalid billing date'
         employee = @company.find_employee_by_id(response[:employee_id])
         project = @company.find_project_by_id(response[:project_id])
         "Invalid Days Worked: \n#{employee.name} worked on #{project.name} on #{response[:date]}, it was a(n) #{response[:reason]}."
@@ -41,7 +51,7 @@ class Audit
       dh = DateHandler::DHDate.new(timesheet.date)
       project = @company.find_project_by_id(timesheet.project_id)
       if !dh.date_between(project.start_date, project.end_date)
-        {project_id: timesheet.project_id, employee_id: timesheet.employee_id, reason: 'invalid billing date'}
+        {project_id: timesheet.project_id, employee_id: timesheet.employee_id, date:timesheet.date, reason: 'invalid billing date'}
       else
         nil
       end
--- test/audit_test.rb
+++ test/audit_test.rb
@@ -62,16 +62,44 @@ class AuditTest < MiniTest::Test
     assert_equal expected, actual
   end
 
-  def test_it_can_return_invalid_days_worked
-    c1 = Company.new
-    c1.load_projects('./data/projects.csv')
-    c1.load_timesheets('./data/weekend_timesheet.csv')
-    c1.load_employees('./data/employees.csv')
-    @a.load_company(c1)
+  def test_it_can_return_invalid_days_worked_for_weekends
+    c = Company.new
+    c.load_projects('./data/projects.csv')
+    c.load_timesheets('./data/weekend_timesheet.csv')
+    c.load_employees('./data/employees.csv')
+    @a.load_company(c)
 
     expected = "Invalid Days Worked: \nSusan Smith worked on Widgets on 2016-02-06, it was a(n) weekend."
     actual = @a.were_invalid_days_worked
 
     assert_equal expected, actual
   end
+
+  def test_it_can_return_invalid_days_worked_for_invalid_billing
+    c = Company.new
+    c.load_projects('./data/projects.csv')
+    c.load_timesheets('./data/invalid_billing_timesheets.csv')
+    c.load_employees('./data/employees.csv')
+    @a.load_company(c)
+
+    expected = "Invalid Days Worked: \nJohn Smith worked on Widgets on 2012-02-01, it was a(n) invalid billing date."
+    actual = @a.were_invalid_days_worked
+
+    assert_equal expected, actual
+  end
+
+  def test_it_can_return_none_if_there_are_no_invalid_days
+    c = Company.new
+    c.load_projects('./data/projects.csv')
+    c.load_timesheets('./data/good_timesheets.csv')
+    c.load_employees('./data/employees.csv')
+    @a.load_company(c)
+
+    expected = "Invalid Days Worked: \nNone"
+    actual = @a.were_invalid_days_worked
+
+    assert_equal expected, actual
+  end
+
+
 end
