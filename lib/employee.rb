8b6bbc848cf6502c23ce866fc14ba6bfa8dbcd80
Judson Stevens <Judson.Stevens@gmail.com>
2018-04-17 09:18:44 -0600

    Working on setting up employee class and tests, WIP

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,37 @@
 class Employee
+
+  def initialize(employee_id, name, role, start_date, end_date)
+    @employee_id = employee_id
+    @name = name
+    @role = role
+    @start_date = start_date
+    @end_date = end_date
+  end
+
 end
+
+
+
+
+
+# require './lib/employee'
+#
+# employee_id = '5'
+# name = 'Sally Jackson'
+# role = 'Engineer'
+# start_date = '2015-01-01'
+# end_date = '2018-01-01'
+# employee = Employee.new(employee_id, name, role, start_date, end_date)
+# => #<Employee...>
+#
+# # make sure to convert all ID values to integers
+# employee.id.class
+# => Integer
+# employee.name
+# => "Sally Jackson"
+# employee.role
+# => "Engineer"
+# employee.start_date
+# => #<Date: 2015-01-01 ((2457024j,0s,0n),+0s,2299161j)>
+# employee.end_date
+# => #<Date: 2018-01-01 ((2458120j,0s,0n),+0s,2299161j)>
--- test/employee_test.rb
+++ test/employee_test.rb
@@ -2,7 +2,11 @@ require './test/test_helper'
 require './lib/employee'
 
 class EmployeeTest < Minitest::Test
+
+  def setup
+    @e = Employee.new('5', 'Sally Jackson', 'Engineer', '2015-01-01', '2018-01-01')
   def test_instantiation
+
   end
 
   def test_attributes
