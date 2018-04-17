0b7bf30ba360c4ff1d03e76495f2826a511cfd97
Judson Stevens <Judson.Stevens@gmail.com>
2018-04-17 09:24:47 -0600

    Implementation of employee class and tests

--- test/employee_test.rb
+++ test/employee_test.rb
@@ -5,10 +5,36 @@ class EmployeeTest < Minitest::Test
 
   def setup
     @e = Employee.new('5', 'Sally Jackson', 'Engineer', '2015-01-01', '2018-01-01')
-  def test_instantiation
+  end
 
+  def test_instantiation
+    assert_instance_of Employee, @e
   end
 
   def test_attributes
+    expected = 5
+    actual = @e.employee_id
+
+    assert_equal expected, actual
+
+    expected = 'Sally Jackson'
+    actual = @e.name
+
+    assert_equal expected, actual
+
+    expected = 'Engineer'
+    actual = @e.role
+
+    assert_equal expected, actual
+
+    expected = Date
+    actual = @e.start_date.class
+
+    assert_equal expected, actual
+
+    expected = Date
+    actual = @e.end_date.class
+
+    assert_equal expected, actual
   end
 end
